88ef855840a4e0ab6a70a267f72dfb6ddebdcaab
Marco <manhani@gmail.com>
2019-11-18 16:13:31 +0000

    Auth done, first commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,70 @@
-'use strict';
+"use strict";
 
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+const User = require("./../models/user");
+const bcryptjs = require("bcryptjs");
+
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
+//Sign In
+router.get("/sign-in", (req, res, next) => {
+  res.render("sign-in");
+});
+
+router.post("/sign-in", (req, res, next) => {
+  let userId;
+  const { name, password } = req.body;
+  User.findOne({ name })
+    .then(user => {
+      if (!user) {
+        return Promise.reject(new Error("There's no user with that username"));
+      } else {
+        userId = user._id;
+        return bcryptjs.compare(password, user.passwordHash);
+      }
+    })
+    .then(result => {
+      if (result) {
+        console.log("Logged in");
+        req.session.user = userId;
+        res.redirect("/");
+      } else {
+        return Promise.reject(new Error("Wrong Password."));
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+//Sign up here
+router.get("/sign-up", (req, res, next) => {
+  res.render("sign-up");
+});
+
+router.post("/sign-up", (req, res, next) => {
+  const { name, password } = req.body;
+  console.log(req.body);
+  bcryptjs
+    .hash(password, 10)
+    .then(hash => {
+      return User.create({
+        name,
+        passwordHash: hash
+      });
+    })
+    .then(user => {
+      console.log("Created", user);
+      req.session.user = user._id;
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
+      next(error);
+    });
+});
 module.exports = router;
